86e68aa416523620fd3138770956f3a085682c19
MR <miguelromerop93@gmail.com>
2021-11-15 20:05:16 -0500

    working on background

---
+++ classes/background.py
@@ -0,0 +1,6 @@
+import pygame, sys
+from pygame.locals import *
+
+posBackground = (0,0)
+background00 = pygame.image.load("./images/background00.png")
+front00 = pygame.image.load("./images/front00.png")
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame, sys
 from pygame.locals import *
+import classes.background as background
 
 pygame.init()
 
-# background ----
-posBackground = (0,0)
-background00 = pygame.image.load("./images/background00.png")
-front00 = pygame.image.load("./images/front00.png")
+# # background ----
+# posBackground = (0,0)
+# background00 = pygame.image.load("./images/background00.png")
+# front00 = pygame.image.load("./images/front00.png")
 
 DISPLAYSURF = pygame.display.set_mode((1200,800))
 pygame.display.set_caption('Yu-Gi-Oh! Reset')
@@ -14,8 +15,9 @@ pygame.display.set_caption('Yu-Gi-Oh! Reset')
 while True:
     
     # DISPLAYSURF.blit(background00, (posBackground))
-    DISPLAYSURF.blit(front00, (posBackground))
+    DISPLAYSURF.blit(background.front00, (background.posBackground))
 
+    # detecting input ------
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
