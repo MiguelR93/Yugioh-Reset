1903d82ab50c63cc35662c8ce9ff54486dc522e4
MR <miguelromerop93@gmail.com>
2021-11-15 19:54:22 -0500

    added new background

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from pygame.locals import *
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((600,450))
+# background ----
+posBackground = (0,0)
+background00 = pygame.image.load("./images/background00.png")
+front00 = pygame.image.load("./images/front00.png")
+
+DISPLAYSURF = pygame.display.set_mode((1200,800))
 pygame.display.set_caption('Yu-Gi-Oh! Reset')
 
 while True:
+    
+    # DISPLAYSURF.blit(background00, (posBackground))
+    DISPLAYSURF.blit(front00, (posBackground))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
