b1995534dc810c496da82bb3abff1595c9c61ea1
MR <miguelromerop93@gmail.com>
2021-11-16 23:21:00 -0500

    added npc class

--- classes/characters.py
+++ classes/characters.py
@@ -1,14 +1,41 @@
+from typing import get_type_hints
 import pygame, sys
 from pygame.locals import *
 
 class Character():
-    # general:
     def __init__(self, id, name, playerX, playerY, avatar):
+        # general:
         self.id = id
         self.name = name
         self.avatar = avatar
         self.playerX = playerX
         self.playerY = playerY
+
+        # on duel:
+        self.oponent = None
+        self.lp = 8000
+        self.victory = None
+
+        # gameZone:
+        self.deck = []
+        self.hand = []
+        self.gy = []
+        self.checkCards = []
+        self.rigthMz = None
+        self.centerMz = None
+        self.leftMz = None
+        self.fieldSCZ = None
+        self.rigthSTZ = None
+        self.centerSTZ = None
+        self.leftSTZ = None
+
+class Npc(Character):
+    def __init__(self, id, name, playerX, playerY, avatar, firstFrase, duelFrase):
+        super().__init__(id, name, playerX, playerY, avatar)
+        
+        #  on Street:
+        self.firstFrase = firstFrase
+        self.duelFrase = duelFrase
         
 
 class Player(Character):
