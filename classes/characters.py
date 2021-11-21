ffbad28e12484f02aeea7e9f8d8ffba400bbc24c
MR <miguelromerop93@gmail.com>
2021-11-20 23:38:20 -0500

    added draw phase to characters

--- classes/characters.py
+++ classes/characters.py
@@ -31,6 +31,27 @@ class Character():
         self.centerSTZ = None
         self.leftSTZ = None
 
+    def drawPhase(self):
+        if self.cardsInHand() >= 5:
+            self.drawACard()
+        else:
+            while (len(self.hand) < 5) and (self.victory == True) and (self.oponent.victory == True): # el anterior era: while (len(self.hand) < 5) and (duel.victory() == True):
+                self.drawACard()
+
+
+    def cardsInHand(self):
+        return len(self.hand)
+
+
+    def drawACard(self):
+        if len(self.deck) == 0:
+            print("No hay cartas en el deck")
+            self.victory = False
+        else:
+            self.hand.append(self.deck[0]) # añade la carta del deck a la mano
+            self.deck.remove(self.deck[0]) # quita del deck la carta añadida a la mano
+
+
 class Npc(Character):
     def __init__(self, id, name, playerX, playerY, avatarFront, avatarRight, avatarLeft, avatarBack, firstFrase, duelFrase):
         super().__init__(id, name, playerX, playerY, avatarFront, avatarRight, avatarLeft, avatarBack)
